3ab96b4c7badf7153a0cd0321d8da3a0e31536a9
DanielZornek <daniel_zornek91@outlook.com>
2024-09-21 12:22:05 -0300

    Updated page styling | ready to start the js logic

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-alert("hello").
+let valueOne;
+let valueTwo;
+let operator;
+
+function takeValues(){
+
+}
+
+function add(){
+
+}
+
+function subtract(){
+
+}
+
+function multiply(){
+
+}
+
+function divide(){
+
+}
